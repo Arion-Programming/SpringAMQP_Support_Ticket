665e0361530a9e7dbee6984fc43d3ddc8b89a1ca
Siegfried Stumpfer | Arion <siegfried.stumpfer@gmail.com>
2024-03-23 18:31:24 +0100

    Initial commit

---
+++ src/main/java/com/springexamples/springamqp_support_ticket/SpringAmqpSupportTicketApplication.java
@@ -0,0 +1,13 @@
+package com.springexamples.springamqp_support_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmqpSupportTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAmqpSupportTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springexamples/springamqp_support_ticket/SpringAmqpSupportTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexamples.springamqp_support_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAmqpSupportTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
